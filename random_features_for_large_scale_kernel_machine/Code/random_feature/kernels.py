ff4a34bc761c048395022287f0d65a5cf0abc0c8
Blanca <blancacano2.71828@gmail.com>
2023-03-08 09:37:43 +0100

    Generate random from a p denisty probability function This closes #13
    Include doctest

--- random_features_for_large_scale_kernel_machine/Code/random_feature/kernels.py
+++ random_features_for_large_scale_kernel_machine/Code/random_feature/kernels.py
@@ -26,30 +26,54 @@ def gaussian_kernel(x:np.array,y:np.array) -> float:
 
     Notes
     -------
-    Alternative parametrization (e.g. en sklearn)
-    gamma = 0.5 / ls**2
+    The formula comes from the article: 
+    author = {Rahimi, Ali and Recht, Benjamin},
+    booktitle = {Advances in Neural Information Processing Systems},
+    editor = {J. Platt and D. Koller and Y. Singer and S. Roweis},
+    publisher = {Curran Associates, Inc.},
+    title = {Random Features for Large-Scale Kernel Machines},
+    url = {https://proceedings.neurips.cc/paper/2007/file/013a006f03dbc5392effeb8f18fda755-Paper.pdf},
+    volume = {20},
+    year = {2007}
+
     Example
     -------
     >>> import numpy as np
     >>> import matplotlib.pyplot as plt
-    >>> import gaussian_process_regression as gp
     >>> X = np.array([[1,2], [3, 4], [5,6]])
-    >>> X_prime = np.array([[1,2], [3, 4]])
-    >>> A, l = 3, 10.0
-    >>> kernel_matrix = gp.rbf_kernel(X, X_prime, A, l)
+    >>> Y= np.array([[1,2], [3, 4]])
+    >>> kernel_matrix = gaussian_kernel(X,Y)
     >>> print(kernel_matrix)
+    [[1.00000000e+00 1.83156389e-02]
+     [1.83156389e-02 1.00000000e+00]
+     [1.12535175e-07 1.83156389e-02]]
     """
     d = distance.cdist(x, y, metric='euclidean')
     return np.exp(-0.5 * (d)**2)
 
 
-def gaussian_fourier_tranform(w: np.array)-> float:
-    '''
-    W array 
+def gaussian_fourier_transform(w: np.array)-> float:
+    ''' Compute the gaussian fourier transform
+    Parameters
+    ----------
+    w: vector from R^d
+
+    Returns
+    -------
+    Gausssian fourier transform of w
+
+    Example
+    -------
+    >>> import numpy as np
+    >>> w = np.array([1,2,3,4])
+    >>> gaussian_fourier_transform(w)
+    7.748596298045689e-09
     ''' 
     D = len(w)
     norm = w.dot(w)
 
-    return (2*np.pi)**(-D/2)* np.exp(norm/2)
+    return (2*np.pi)**(-D/2)* np.exp(-norm/2)
 
-
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
